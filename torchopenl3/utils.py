de7837a58655834f4785ce606ec71ffbcb00de3c
Luckygyana <gyanendralucky9337@gmail.com>
2021-03-18 14:10:19 +0530

    Update utils.py

--- torchopenl3/utils.py
+++ torchopenl3/utils.py
@@ -1,27 +1,26 @@
 import numpy as np
 import resampy
+import openl3
+from keras import Model, Input
 
 TARGET_SR = 48000
 
-
 def center_audio(audio, frame_len):
-    return np.pad(audio, (int(frame_len / 2.0), 0), mode="constant", constant_values=0)
-
+    return np.pad(audio, (int(frame_len / 2.0), 0), mode='constant', constant_values=0)
 
 def pad_audio(audio, frame_len, hop_len):
     audio_len = audio.size
     if audio_len < frame_len:
         pad_length = frame_len - audio_len
     else:
-        pad_length = int(
-            np.ceil((audio_len - frame_len) / float(hop_len))
-        ) * hop_len - (audio_len - frame_len)
+        pad_length = int(np.ceil((audio_len - frame_len)/float(hop_len))) * hop_len \
+            - (audio_len - frame_len)
 
     if pad_length > 0:
-        audio = np.pad(audio, (0, pad_length), mode="constant", constant_values=0)
+        audio = np.pad(audio, (0, pad_length),
+                       mode='constant', constant_values=0)
 
     return audio
-
 
 def get_num_windows(audio_len, frame_len, hop_len, center):
     if center:
@@ -30,7 +29,7 @@ def get_num_windows(audio_len, frame_len, hop_len, center):
     if audio_len <= frame_len:
         return 1
     else:
-        return 1 + int(np.ceil((audio_len - frame_len) / float(hop_len)))
+        return 1 + int(np.ceil((audio_len - frame_len)/float(hop_len)))
 
 
 def preprocess_audio_batch(audio, sr, center=True, hop_size=0.1):
@@ -39,8 +38,7 @@ def preprocess_audio_batch(audio, sr, center=True, hop_size=0.1):
 
     if sr != TARGET_SR:
         audio = resampy.resample(
-            audio, sr_orig=sr, sr_new=TARGET_SR, filter="kaiser_best"
-        )
+            audio, sr_orig=sr, sr_new=TARGET_SR, filter='kaiser_best')
 
     audio_len = audio.size
     frame_len = TARGET_SR
@@ -52,12 +50,16 @@ def preprocess_audio_batch(audio, sr, center=True, hop_size=0.1):
     audio = pad_audio(audio, frame_len, hop_len)
 
     n_frames = 1 + int((len(audio) - frame_len) / float(hop_len))
-    x = np.lib.stride_tricks.as_strided(
-        audio,
-        shape=(frame_len, n_frames),
-        strides=(audio.itemsize, hop_len * audio.itemsize),
-    ).T
+    x = np.lib.stride_tricks.as_strided(audio, shape=(frame_len, n_frames),
+                                        strides=(audio.itemsize, hop_len * audio.itemsize)).T
 
     x = x.reshape((x.shape[0], 1, x.shape[-1]))
-
-    return x
+    
+    tf_model = openl3.models.load_audio_embedding_model(
+        input_repr=input_repr, content_type=content_type, embedding_size=embedding_size)
+    inp = tf_model.get_layer(f'input_2').input
+    oups = tf_model.get_layer(f'melspectrogram_2').output
+    model_mel = Model(inputs=[inp], outputs=oups)
+    x = model_mel.predict(x)
+    
+    return torch.Tensor(x.swapaxes(2, 3).swapaxes(1, 2))
