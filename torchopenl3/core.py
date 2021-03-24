6af0d238342aa48a01a205b5a0fa5be86c00fd21
Joseph Turian <turian@gmail.com>
2021-03-24 23:55:15 +0100

    Prints

--- torchopenl3/core.py
+++ torchopenl3/core.py
@@ -121,6 +121,7 @@ def get_audio_embedding(
             for i in range((total_size // batch_size) + 1):
                 small_batch = audio[i * batch_size : (i + 1) * batch_size]
                 if small_batch.shape[0] > 0:
+                    print(small_batch.shape)
                     audio_embedding.append(model(small_batch))
         audio_embedding = torch.stack(audio_embedding)
         if audio.is_cuda:
